eeca1f03a6b1cc12f316aeb11c49559052408802
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2025-06-04 12:34:04 -0300

    Start to use fragment

---
+++ app/src/main/java/com/picpay/desafio/android/ContactsFragment.kt
@@ -0,0 +1,106 @@
+package com.picpay.desafio.android
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.picpay.desafio.android.databinding.FragmentContactsBinding
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ContactsFragment : Fragment(R.layout.fragment_contacts) {
+
+    companion object {
+        fun newInstance() = ContactsFragment()
+    }
+
+    private var _binding: FragmentContactsBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: ContactsViewModel by viewModels()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var adapter: UserListAdapter
+
+    private val url = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
+
+    private val gson: Gson by lazy { GsonBuilder().create() }
+
+    private val okHttp: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(url)
+            .client(okHttp)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    private val service: PicPayService by lazy {
+        retrofit.create(PicPayService::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentContactsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        recyclerView = binding.recyclerView
+        progressBar = binding.userListProgressBar
+
+        adapter = UserListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        progressBar.visibility = View.VISIBLE
+        service.getUsers()
+            .enqueue(object : Callback<List<User>> {
+                override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                    val message = getString(R.string.error)
+
+                    progressBar.visibility = View.GONE
+                    recyclerView.visibility = View.GONE
+
+                    Toast.makeText(context, message, Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                    progressBar.visibility = View.GONE
+
+                    adapter.users = response.body()!!
+                }
+            })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/picpay/desafio/android/ContactsViewModel.kt
@@ -0,0 +1,7 @@
+package com.picpay.desafio.android
+
+import androidx.lifecycle.ViewModel
+
+class ContactsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
