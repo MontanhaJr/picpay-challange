bd0b374b5cf6fc389059af6cd72a58ed746a88ad
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2025-06-04 14:04:12 -0300

    Implements navigation

--- app/src/main/java/com/picpay/desafio/android/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/MainActivity.kt
@@ -3,14 +3,9 @@ package com.picpay.desafio.android
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.bundle.Bundle
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.contacts_fragment, ContactsFragment())
-            .commit()
     }
 }
